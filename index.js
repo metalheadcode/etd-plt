3efdf89daa43eeb6fbe2f70e37bd35f9a7efd5a2
Ahmad Hazim <metalheadcoder@gmail.com>
2022-03-17 17:58:42 +0800

    update edt to etd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const findByCountryCode = (countryCode, plt = true) => {
   if (plt) {
     return countryList.plt.find((item) => item.countryCode === countryCode);
   } else {
-    return countryList.edt.find((item) => item.countryCode === countryCode);
+    return countryList.etd.find((item) => item.countryCode === countryCode);
   }
 };
 
